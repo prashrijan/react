c7dc62324109b9955ec453e1cbeeda74c535ea99
Prashrijan <shresthaprashrijan@gmail.com>
2024-09-21 13:19:43 +1000

    day 5 of react

---
+++ stateBasic/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import {sculptureList} from './data.js'
+import './App.css'
+function App() {
+  const[index, setIndex] = useState(0);
+  const[showMore, setShowMore] = useState(false)
+
+  let sculpture = sculptureList[index]
+
+  function handleClick(){
+    setIndex(index + 1)    
+  }
+  function handleMoreClick(){
+    setShowMore(prev => !prev)
+  }
+
+  return (
+    <>
+    <div 
+      className="container flex items-center h-screen w-full justify-center bg-slate-700 flex-col text-white gap-3">
+      <p>Image of ({index + 1} of {sculptureList.length})</p>
+      <h2 
+        className='text-2xl'>
+          {sculpture.name}
+      </h2>
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt} />
+      <h3 className='text-xl'>{sculpture.artist}</h3>
+      <button 
+        className='underline cursor-pointer text-white px-3 py-2 rounded-lg text-xl '
+        onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p className='w-2/4 text-center'>{sculpture.description}</p>}    
+      <button 
+        className='bg-orange-500 text-white px-3 py-2 rounded-lg text-xl '
+        onClick={handleClick}>
+        Next
+      </button>
+    </div>
+    </>
+  )
+}
+
+export default App
