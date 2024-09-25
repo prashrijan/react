9210ac3067343da8695f3a7e7c72071ccab1f34e
Prashrijan <shresthaprashrijan@gmail.com>
2024-09-25 22:55:07 +1000

    context api min

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,16 @@
+import Login from "./components/Login/Login"
+import Profile from "./components/Profile/Profile"
+import UserContextProvider from "./context/UserContextProvider"
+
+function App() {
+
+
+  return (
+    <UserContextProvider>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
